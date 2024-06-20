c577b29ca6921d2b0c0d86d1fcf9cbf9cb739783
T0ffen <19leogal99@gmail.com>
2024-06-21 03:19:07 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+Total_tasks_done = 12
+Hours_spent = 1.5
+Course = 'Python'
+Time_for_one_task = Hours_spent / Total_tasks_done
+print('Курс:', Course + ',', 'всего задач:', Total_tasks_done, ', затрачено часов:', Hours_spent,
+      ', среднее время выполнения', Time_for_one_task, 'часа.')
